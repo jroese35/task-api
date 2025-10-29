2545e2ad8f2bf22af74ff4cba30f94a61c1f853f
John Roese <jroese@charlotte.edu>
2025-10-28 20:04:43 -0400

    Added get by ID endpoint

--- src/repositories/taskRepo.js
+++ src/repositories/taskRepo.js
@@ -4,6 +4,18 @@ export async function findAll() {
   return prisma.task.findMany();
 }
 
+export async function getById(id) {
+  let task = await prisma.task.findUnique({
+    where: { id: id },
+    select: {
+      id: true,
+      title: true,
+      completed: true
+    }
+  });
+  return task;
+}
+
 // Create a new task
 export async function create(data) {
   return prisma.task.create({
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -4,6 +4,16 @@ export async function getAllTasks() {
   return taskRepository.findAll();
 }
 
+export async function getTaskById(id) {
+  let task = await taskRepository.getById(id);
+  if (task) return task;
+  else {
+    const error = new Error(`Task not found`);
+    error.status = 404;
+    throw error;
+  }
+}
+
 export async function createTask(newTask) {
   return taskRepository.create(newTask);
 }
